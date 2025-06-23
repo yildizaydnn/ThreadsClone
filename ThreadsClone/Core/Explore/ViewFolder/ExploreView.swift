3a6f4705817dc7260df5c93b52ce9a1c3df2002f
yildizaydnn <yildizaydd@gmail.com>
2025-06-23 20:19:41 +0300

    exploreView

--- ThreadsClone/Core/Explore/ViewFolder/ExploreView.swift
+++ ThreadsClone/Core/Explore/ViewFolder/ExploreView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var searchText = ""
     var body: some View {
-        Text("explore")
+        NavigationStack{
+            ScrollView {
+                LazyVStack {
+                    ForEach(0...10, id: \.self) { user in
+                        VStack {
+                            HStack {
+                                Image("profile1")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .clipShape(Circle())
+                                
+                                VStack(alignment: .leading){
+                                    Text("coolcat1")
+                                        .font(.footnote)
+                                        .fontWeight(.semibold)
+                                    
+                                    Text("cat cat")
+                                    
+                                }
+                                .font(.footnote)
+                                
+                                Spacer()
+                                
+                                Text("Follow")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .frame(width: 100, height: 32)
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color(.systemGray),lineWidth: 1)
+                                    }
+                            }
+                            
+                            .padding(.horizontal)
+                            
+                            Divider()
+                        }
+                        .padding(.vertical, 4)
+        }
+                }
+            }
+            .searchable(text: $searchText, prompt: "Search")
+        }
     }
 }
 
